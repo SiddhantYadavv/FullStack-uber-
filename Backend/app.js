75d541f64e7ac7365233453f0b5c8d1c19e0e5b3
Siddhant <sidhantyadav1999@gmail.com>
2025-05-28 22:22:19 +0530

    Setup socket.io for backend

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from "express"
 import cors from "cors"
+import http from "http"
 import { connectToDb } from './DB/db.js';
 import { UserRouter } from './Routes/user.routes.js';
 import { captainRouter } from './Routes/captain.routes.js';
 import  {mapRouter} from "./Routes/maps.routes.js"
 import { rideRouter } from './Routes/ride.routes.js';
 import cookieParser from 'cookie-parser';
+import {initializeSocket} from "./socket.js"
 
 const app = express()
 app.use(cors())
@@ -22,6 +24,11 @@ app.use("/rides",rideRouter)
 
 
 connectToDb()
+
+const server = http.createServer(app);
+initializeSocket(server);
+
+
 //remove "0.0.0.0" later
 app.listen(process.env.PORT,'0.0.0.0',()=>{
     console.log("Server is running on port:",process.env.PORT)
